efbe6ef3a8c81caf591394b5c5dd733178d5861c
avishkakavindu <avishkakavindud@gmail.com>
2022-08-22 16:14:11 +0530

    Looping through list in template

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 module.exports = (params) => {
     // index page
     router.get('/', (request, response) => {
-        response.render('pages/index', { pageTitle: 'Welcome' });
+        response.render('layouts', { pageTitle: 'Welcome', template: 'index' });
     });
 
     router.use('/speakers', speakersRoute(params));
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
@@ -12,14 +13,34 @@ const routes = require('./routes/index');
 // instance of express
 const app = express();
 
+app.set('trust proxy', 1);
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['Dkjvnsblu', 'HBbdhbbj'],
+}));
+
 // set view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
+
+app.locals.siteName = 'ROUX Meetups';
 
 // Middlewares
 // - static file paths
 app.use(express.static(path.join(__dirname, './static')));
 
+// global template variable
+app.use(async (request, response, next) => {
+    try {
+        const names = await speakersService.getNames();
+        response.locals.speakerNames = names;
+
+        return next();
+    } catch (error) {
+        return next(error);
+    }
+});
 app.use('/', routes({
     feedbackService,
     speakersService
